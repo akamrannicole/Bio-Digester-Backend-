4d2f171fb6532b63841a7725be9520dfa738c3ab
user.name <akamrannicole@gmail.com>
2025-02-13 12:56:23 +0300

    cors

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
+from flask import Flask
+from flask_cors import CORS
 from app import create_app, db
 from app.models import User, Product, Order, Receipt
 
 app = create_app()
+CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:5173"}})
 
 @app.shell_context_processor
 def make_shell_context():
@@ -9,3 +12,4 @@ def make_shell_context():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
